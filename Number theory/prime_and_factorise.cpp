0ae73961b7f4306dfcbcc02e8e578a8afab3e9d1
Alyona-5446 <75212534+Alyona-5446@users.noreply.github.com>
2021-06-22 08:55:52 +0300

    Add files via upload

---
+++ Number theory/prime_and_factorise.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+bool is_prime(int n) {
+    if (n == 2) {
+        return true;
+    }
+    if (!(n & 1)) {
+        return false;
+    }
+    for (int i = 3; i * i <= n; i += 2) {
+        if (!(n % i)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+auto factorise(int n) {
+    std::vector<std::pair<int, int>> factors;
+    if (!(n & 1)) {
+        int p = 0;
+        while (!(n & 1)) {
+            n >>= 1;
+            ++p;
+        }
+        factors.emplace_back(2, p);
+    }
+    for (int i = 3; i * i <= n; i += 2) {
+        if (!(n % i)) {
+            int p = 0;
+            while (!(n % i)) {
+                n /= i;
+                ++p;
+            }
+            factors.emplace_back(i, p);
+        }
+    }
+    if (n > 1) {
+        factors.emplace_back(n, 1);
+    }
+    return factors;
+}
+
+auto all_divs(int n) {
+    std::vector<int> divs;
+    int i;
+    for (i = 1; i * i < n; ++i) {
+        if (!(n % i)) {
+            divs.push_back(i);
+            divs.push_back(n / i);
+        }
+    }
+    if (i * i == n) {
+        divs.push_back(i);
+    }
+    std::sort(divs.begin(), divs.end());
+    return divs;
+}
+
+int main() {
+    return 0;
+}
